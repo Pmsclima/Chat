1cc7f3c804094f5bede44cce139a2639b7ff9fc4
Pedro Lima <pmsclima@gmail.com>
2019-06-20 18:46:18 +0100

    Chat Created

---
+++ src/org/academiadecodigo/bootcamp/chat/ClientChat.java
@@ -0,0 +1,56 @@
+package org.academiadecodigo.bootcamp.chat;
+
+/**
+ * @author Pedro Lima
+ * @version 1 Jun 20, 2019
+ */
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientChat {
+
+    public static void main(String[] args) throws IOException {
+
+        int portNumber = 8080;
+        String hostName = "127.0.0.1";
+        String messageIn = " ";
+        String messageOut = " ";
+
+
+        /**
+         * Socket is our client
+         */
+
+        Socket clientSocket = new Socket(hostName, portNumber);
+
+        /**
+         * DataInputStream - read data
+         * DataOutputStream - write data
+         */
+
+        DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+        /**
+         * BufferedReader - read text from a character-input stream
+         */
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (!messageIn.equalsIgnoreCase("Bye")){
+
+          messageOut = bufferedReader.readLine();
+          dataOutputStream.writeUTF(messageOut);
+          messageIn =  dataInputStream.readUTF();
+          System.out.println(messageIn); // Print server message
+        }
+
+        /**
+         * Close() - Terminate a communications between 2 machines
+         */
+
+        clientSocket.close();
+
+    }
+}
---
+++ src/org/academiadecodigo/bootcamp/chat/ServerChat.java
@@ -0,0 +1,62 @@
+package org.academiadecodigo.bootcamp.chat;
+
+/**
+ * @author Pedro Lima
+ * @version  1 Jun 20, 2019
+ */
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerChat {
+
+    public static void main(String[] args) throws IOException {
+
+        int portNumber = 8080;
+        String messageIn = " ";
+        String messageOut = " ";
+
+
+        /**
+         * Declare and instantiate a Server Socket
+         * Server Socket will wait for conection of client
+         * Socket is our client and method Accpept() listen for a connection and accept if any found.
+         * Accept() blocks all the client that is made, it is waiting for someone to connect.
+         */
+
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        Socket clientSocket = serverSocket.accept();
+
+        /**
+         * DataInputStream - read data
+         * DataOutputStream - write data
+         */
+
+        DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+        /**
+         * BufferedReader - read text from a character-input stream
+         */
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (!messageIn.equalsIgnoreCase("Bye")){
+
+            messageIn = dataInputStream.readUTF();
+            System.out.println(messageIn); // Print client message
+            messageOut = bufferedReader.readLine();
+            dataOutputStream.writeUTF(messageOut);
+
+        }
+
+        /**
+         * Close() - Terminate a communications between 2 machines
+         */
+
+        clientSocket.close();
+
+    }
+
+}
